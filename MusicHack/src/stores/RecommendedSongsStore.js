a46729dec4964fb696a7663325917ae20fdfc875
Collin <dever.cj@gmail.com>
2020-02-24 16:11:27 -0800

    deleted the flutter application.  Added alt.js which uses Flux facebook's design pattern for handling data.  See the video on the site https://facebook.github.io/flux/ for more information. Built out the basics project structure using an alt.js demo app here https://github.com/mrblueblue/react-native-alt-demo

---
+++ MusicHack/src/stores/RecommendedSongsStore.js
@@ -0,0 +1,18 @@
+import alt from '../alt';
+import RecommendedSongsAction from '../actions/RecommendedSongsActions';
+
+export class RecommendedSongsStore {
+  constructor() {
+    this.songs = [];
+
+    this.bindListeners({
+      handleUpdateSongs: RecommendedSongsAction.UPDATE_RECOMMENDED_SONGS,
+    });
+  }
+
+  handleUpdateSongs(songs) {
+    this.songs = songs;
+  }
+}
+
+export default alt.createStore(RecommendedSongsStore, 'RecommendedSongsStore');
