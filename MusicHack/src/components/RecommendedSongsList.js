2af67bca6d1e671ff4922ee5ed0e1e53ed4fa3b1
Collin <dever.cj@gmail.com>
2020-03-01 11:59:30 -0800

    hooked up the action and store to a listview that displays song title

---
+++ MusicHack/src/components/RecommendedSongsList.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react';
+import {FlatList, StyleSheet} from 'react-native';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+import RecommendedSongRow from './RecommendedSongRow';
+import RecommendedSongsStore from '../stores/RecommendedSongsStore';
+import RecommendedSongsActions from '../actions/RecommendedSongsActions';
+
+class RecommendedSongsList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = RecommendedSongsStore.getState();
+    this.onChange = this.onChange.bind(this);
+  }
+
+  componentDidMount() {
+    RecommendedSongsStore.listen(this.onChange);
+    RecommendedSongsActions.getTestSongs();
+  }
+
+  componentWillUnmount() {
+    RecommendedSongsStore.unlisten(this.onChange);
+  }
+
+  onChange(state) {
+    this.setState(state);
+  }
+
+  render() {
+    return (
+      <FlatList
+        contentInsetAdjustmentBehavior="automatic"
+        style={styles.scrollView}
+        data={this.state.songs}
+        renderItem={({item}) => <RecommendedSongRow title={item.title} />}
+      />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+});
+
+export default RecommendedSongsList;
