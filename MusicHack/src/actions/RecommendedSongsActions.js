48b043a09bd36cf13875529d7c67805bbd1c3b12
Dmytro <dima220v@gmail.com>
2020-03-04 02:02:17 -0800

    Display top10 artist

--- MusicHack/src/actions/RecommendedSongsActions.js
+++ MusicHack/src/actions/RecommendedSongsActions.js
@@ -7,24 +7,44 @@ class RecommendedSongsActions {
     this.generateActions('updateRecommendedSongs');
   }
 
+  componentDidMount () {
+    return fetch('https://api.deezer.com/chart/0/artists')
+      .then ( (response) => response.json() )
+      .then ( (responseJson) => {
+        console.log(responseJson.total);
+        this.updateRecommendedSongs(responseJson.data);        // console.log(responseJson.total);
+      })
+    .catch((error) => {
+      console.log(error)
+    });
+  }
+  // name
+  // position
+  // type
+
   getTestSongs() {
-    this.updateRecommendedSongs([
-      {
-        artist: 'The Beatles',
-        title: 'Yellow Submarine',
-        genre: 'Classic Rock',
-      },
-      {
-        artist: 'Post Malone',
-        title: 'Circles',
-        genre: 'Rap',
-      },
-      {
-        artist: 'The Weeknd',
-        title: 'Heartless',
-        genre: 'Pop',
-      },
-    ]);
+    this.componentDidMount();
+    // console.log(this.componentDidMount());
+  //   this.updateRecommendedSongs(
+  //     // this.componentDidMount().data
+  //     [
+  //     {
+  //       artist: 'The Beatles',
+  //       title: 'Yellow Submarine',
+  //       genre: 'Classic Rock',
+  //     },
+  //     {
+  //       artist: 'Post Malone',
+  //       title: 'Circles',
+  //       genre: 'Rap',
+  //     },
+  //     {
+  //       artist: 'The Weeknd',
+  //       title: 'Heartless',
+  //       genre: 'Pop',
+  //     },
+  //   ]
+  //   );
   }
 }
 
