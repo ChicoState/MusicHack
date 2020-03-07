251a90908dbc3d969793dd71eea7b39a13e079c3
dtaylor46 <dtaylor647@gmail.com>
2020-03-06 18:00:16 -0800

    Added artist name and cover art

--- MusicHack/src/components/RecommendedSongRow.js
+++ MusicHack/src/components/RecommendedSongRow.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View, Text, Image} from 'react-native';
 
 export default class RecommendedSongRow extends Component {
   render() {
     return (
       <View style={styles.mainView}>
-        <Text>{this.props.title}</Text>
+        <Text>{this.props.title} -- {this.props.artist}</Text>
+		<Image source = {{uri: this.props.artwork}} style = {{width: 300, height: 300}}/>
       </View>
     );
   }
@@ -16,5 +17,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+	marginBottom: 10,
   },
 });
